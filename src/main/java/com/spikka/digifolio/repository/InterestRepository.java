9e1bd72f06a1f7263817a3e4574dd7c2949bbc1a
spikka <flsh666@yandex.ru>
2025-06-15 21:54:29 +0300

    Initial commit: добавление всего кода проекта

--- src/main/java/com/spikka/digifolio/repository/InterestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spikka.digifolio.repository;
-
-import com.spikka.digifolio.model.Interest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface InterestRepository extends JpaRepository<Interest, Long> {
-    List<Interest> findByStudentId(Long studentId);
-}
